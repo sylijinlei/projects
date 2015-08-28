ea2dc980139cfda05bd04e9b2a631e03b821190e
李金磊 <sylijinlei@163.com>
2015-08-28 08:59:35 +0800

    java:add eclise src files

---
+++ java/04_package/src/com/xiaoming/Test.java
@@ -0,0 +1,18 @@
+package com.xiaoming;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is xiaoming's test class!");
+	}
+
+}
+
+class Dog
+{
+	
+}
---
+++ java/04_package/src/com/xiaoqiang/Test.java
@@ -0,0 +1,17 @@
+package com.xiaoqiang;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+class Dog
+{
+	
+}
