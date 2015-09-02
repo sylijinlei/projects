6aeb91d3d0a4f83dfa0437f08cb45ee91a41b20d
李金磊 <sylijinlei@163.com>
2015-09-02 13:26:02 +0800

    java and androoid: modify

--- java/02_static_fun/Var.java
+++ java/02_static_fun/Var.java
@@ -6,17 +6,30 @@ public class Var
 	}
 	public static  void main(String args[])
 	{
-		Stu stu1 = new Stu("小明", 10, 100);
-		Stu stu2 = new Stu("小华", 11, 200);
-		System.out.println("total fee :"+stu1.getTotalFee());
-		System.out.println("child total:"+Child.total);
+		//Stu stu1 = new Stu("小明", 10, 100);
+		//Stu stu2 = new Stu("小华", 11, 200);
+		//System.out.println("total fee :"+stu1.getTotalFee());
+		//System.out.println("child total:"+Child.total);
+		//System.out.println(Stu.ch1.age);
+		try
+		{
+			Stu.ch1.cry();
+
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+
+			System.out.println(e);
+		}
+		System.out.println("i will exit!");
 	}
 }
-class Stu
+//final作为类的成员变量必须在定义时或构造函数中初始化
+final class Stu
 {
 	String name;
 	int age;
-	int fee;
+	int fee ;
+	public static final Child ch1 = new Child();
 	static int totalFee;
 	public Stu(String name, int age, int fee)
 	{
@@ -33,11 +46,15 @@ class Stu
 class Child
 {
 	String name;
-	int age;
+	int age = 12;
 	static int total = 99;
 	static
 	{
 		System.out.println("Child loaded!");
+	}
+	public  void cry()
+	{
+		System.out.println("I am a child!");
 	}
 
 }
