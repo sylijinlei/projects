6fb13e776cc2ae782e8267e66db2ddf69449b1c3
李金磊 <sylijinlei@163.com>
2016-04-10 23:02:49 +1000

    xiu gai socket

--- c/22_socket_udp/client.c
+++ c/22_socket_udp/client.c
@@ -8,28 +8,57 @@
 
 #define UDP_PORT 3434
 
-void main(){
-	int client_fd;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t socket_len;
-	size_t recv_len;
-	char buffer[100];
+void main(int args, char **argv){
+    int client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t socket_len;
+    size_t recv_len;
+    char buffer[100];
+    if(args < 2){
+    	perror("Usage:\n     ./client server_ip");
+    	exit(1);
+    }
+    memset(&server_addr, 0, sizeof(struct sockaddr_in));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(UDP_PORT);
+    server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    
+    memset(&client_addr, 0, sizeof(struct sockaddr_in));
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(3435);
+/*    client_addr.sin_addr.s_addr = inet_addr("192.168.43.170");*/
+      /*  client_addr.sin_addr.s_addr = inet_addr("192.168.3.7");
+*/
 
-	memset(&server_addr, 0, sizeof(struct sockaddr_in));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(UDP_PORT);
-	server_addr.sin_addr.s_addr = inet_addr("192.168.40.134");
+        client_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    /*client_addr.sin_addr.s_addr = htonl(INADDR_ANY);*/
 
-	client_fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if(-1 == client_fd){
-		perror("Create socket failed!\n");
-		exit(1);
-	}
-	char *send_string = "send string\n";
-	strncpy(buffer, send_string, strlen(send_string));
-	if(sendto(client_fd, buffer, strlen(send_string),0, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) < 0){
-		perror("send data failed!\n");
-		exit(1);
-	}
-	close(client_fd);
+
+    client_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(-1 == client_fd){
+        perror("Create socket failed!\n");
+        exit(1);
+    }
+
+    if(bind(client_fd, (struct sockaddr*)&client_addr, sizeof(struct sockaddr)) == -1){
+        perror("bind error!\n");
+        exit(1);
+    }
+
+    char *send_string = "client string\n\0";
+    strncpy(buffer, send_string, strlen(send_string));
+    printf("len = %d\n", strlen(send_string));
+    if(sendto(client_fd, buffer, strlen(send_string),0, (struct sockaddr*)&server_addr, sizeof(struct sockaddr)) < 0){
+        perror("send data failed!\n");
+        exit(1);
+    }
+    if((recv_len = recvfrom(client_fd, buffer, 100, 0, (struct sockaddr*)&server_addr, &socket_len)) == -1){
+    	perror("receive error!\n");
+    	exit(1);
+    }else{
+    	buffer[recv_len] = '\0';
+    	printf("reponse from server, server addr = %s, port = %d, data size = %d, data = %s\n",
+    			inet_ntoa(server_addr.sin_addr), ntohs(client_addr.sin_port), (int)recv_len, buffer);
+    }
+    close(client_fd);
 }
--- c/22_socket_udp/server.c
+++ c/22_socket_udp/server.c
@@ -9,40 +9,44 @@
 #define UDP_PORT 3434
 
 void main(){
-	int server_fd;
-	struct sockaddr_in server_addr, client_addr;
-	socklen_t socket_len;
-	size_t recv_len;
-	char buffer[100];
-	printf("I am receiving data...");
+    int server_fd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t socket_len;
+    size_t recv_len, send_len;
+    char buffer[100];
 
-	memset(&server_addr, 0, sizeof(struct sockaddr_in));
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(UDP_PORT);
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    memset(&server_addr, 0, sizeof(struct sockaddr_in));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(UDP_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    /*server_addr.sin_addr.s_addr = inet_addr("192.168.3.2");*/
 
-	server_fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if(-1 == server_fd){
-		perror("Create socket failed!\n");
-		exit(1);
-	}
-	if((bind(server_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)){
-		perror("Server bind failed!");
-		exit(1);
-	}
+    server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(-1 == server_fd){
+        perror("Create socket failed!\n");
+        exit(1);
+    }
+    if((bind(server_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)){
+        perror("Server bind failed!");
+        exit(1);
+    }
 
     while(1){
-    {
-		printf("I am receiving data...");
-		if((recv_len = recvfrom(server_fd, buffer, 20, 0, (struct sockaddr *)&client_addr, &socket_len)) == -1){
-			perror("Receive data failed!\n");
-			exit(1);
-		}else{
-			printf("Recv data from client, client addr = %s, data size = %d, data = %s\n",
-					inet_ntoa(client_addr.sin_addr), (int)recv_len, buffer);
-		}
 
-
-	}
-
+        printf("I am receiving data...\n");
+        if((recv_len = recvfrom(server_fd, buffer, 20, 0, (struct sockaddr *)&client_addr, &socket_len)) == -1){
+            perror("Receive data failed!\n");
+            exit(1);
+        }else{
+            printf("Recv data from client, client addr = %s,  port = %d, data size = %d, data = %s\n",
+                    inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),  (int)recv_len, buffer);
+        }
+       char *send_string = "server reponse string";
+       send_len = strlen(send_string);
+       strncpy(buffer, send_string, send_len);
+        if(sendto(server_fd, buffer, send_len, 0, (struct sockaddr*)&client_addr, sizeof(struct sockaddr)) == -1){
+            perror("send error!\n");
+            exit(1);
+        }
+    }
 }
